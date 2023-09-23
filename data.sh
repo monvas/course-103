65c7840f440e35ee2e6940661c497f4bbc53fba0
ToddBooth <Git@ToddBooth.Com>
2023-09-23 17:03:43 +0200

    commit

--- data.sh
+++ data.sh
@@ -5,6 +5,9 @@
 # domains for GoDaddy and WordPress sub-sites
 domains=" a01-stud-01.ToddBooth.com a01-stud-02.ToddBooth.com a01-stud-03.ToddBooth.com a01-stud-04.ToddBooth.com a01-stud-05.ToddBooth.com a01-stud-06.ToddBooth.com a01-stud-07.ToddBooth.com a01-stud-08.ToddBooth.com a01-stud-09.ToddBooth.com a01-stud-10.ToddBooth.com a01-stud-11.ToddBooth.com a01-stud-12.ToddBooth.com"
 
+# combined for WordPress certificates and Web site testing
+domains="toddbooth.com d7001d-ziqyou-3.toddbooth.com d7001d-sincho-3.toddbooth.com d7001d-seygha-3.toddbooth.com d7001d-ramyer-3.toddbooth.com d7001d-parala-3.toddbooth.com d7001d-nanbas-3.toddbooth.com d7001d-nafmah-3.toddbooth.com d7001d-muhlar-3.toddbooth.com d7001d-monvas-3.toddbooth.com d7001d-mohsou-3.toddbooth.com d7001d-maharg-3.toddbooth.com d7001d-laurod-3.toddbooth.com d7001d-lasman-3.toddbooth.com d7001d-krikol-3.toddbooth.com d7001d-karsur-3.toddbooth.com d7001d-josomi-3.toddbooth.com d7001d-imaoho-2.toddbooth.com d7001d-hamaln-3.toddbooth.com d7001d-gleloh-3.toddbooth.com d7001d-eyvahm-3.toddbooth.com d7001d-davwas-7.toddbooth.com d7001d-davfol-3.toddbooth.com d7001d-bogpat-3.toddbooth.com d7001d-amoote-3.toddbooth.com d7001d-alrahm-3.toddbooth.com d7001d-adivis-3.toddbooth.com a01-stud-01.ToddBooth.com a01-stud-02.ToddBooth.com a01-stud-03.ToddBooth.com a01-stud-04.ToddBooth.com a01-stud-05.ToddBooth.com a01-stud-06.ToddBooth.com a01-stud-07.ToddBooth.com a01-stud-08.ToddBooth.com a01-stud-09.ToddBooth.com a01-stud-10.ToddBooth.com a01-stud-11.ToddBooth.com a01-stud-12.ToddBooth.com"
+
 # Specify the sub-domain you want to test
 # domains="toddbooth.com"
 
