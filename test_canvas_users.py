4f2de480180c6b3b33982a708751afd4d0b28a5e
ToddBooth <Git@ToddBooth.Com>
2023-09-25 12:40:44 +0200

    commit

--- test_canvas_users.py
+++ test_canvas_users.py
@@ -1,38 +1,57 @@
+import json
 import os
 import requests
 import sys
+
+# required Environment Variables
+# CANVAS_TOKEN
 
 # Get the value of the "token" environment variable
 token = os.environ.get("CANVAS_TOKEN")
 
 # Check if the variable is set
-if token is not None:
-    print("token:", token)
-else:
+if token is None:
     print("Canvas token environment variable is not set.")
     sys.exit(1)
+else:
+    print("token: found")
 
 headers = {
     "Authorization": "Bearer " + token,
 }
 
-course_id = 19899
+# network programming
+BASE_URL = "https://canvas.ltu.se/api/v1"
+COURSE_ID = 19899
 
-r = requests.get(
-    f"https://canvas.ltu.se/api/v1/courses/{course_id}/users",
+response = requests.get(
+    f"{BASE_URL}/courses/{COURSE_ID}/users",
     headers=headers)
 
-print(r.status_code)
+print(response.status_code)
 
 # Check if the response status code is 200 (OK) before attempting to parse the JSON response
-if r.status_code == 200:
-    users_data = r.json()
-    for user in users_data:
-        # Access the 'name' key within each user dictionary
-        print(user['name'])
-
-        # for now, exit after outputting the first user
-        sys.exit(0)
-else:
-    print(f"Request failed with status code {r.status_code}")
+if response.status_code != 200:
+    print(f"Request failed with status code {response.status_code}")
     sys.exit(1)
+
+users_data = response.json()
+
+for user in users_data:
+
+    # Pretty print the JSON data
+    # print(json.dumps(user, indent=4))
+
+    # Access the 'name' key within each user dictionary
+    # print(user['name'])
+
+    # Access the 'sis_user_id' key within each user dictionary
+    # print(user['sis_user_id'])
+
+    # Access the 'sis_user_id' key within each user dictionary and verify that it is "maharg-3"
+    if user['sis_user_id'] == "maharg-3":
+        print(user['sis_user_id'])
+        sys.exit(1)
+
+print("No user with sys_user_id 'maharg-3' found.")
+sys.exit(0)
---
+++ test_canvas_users.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Get list of Wordpress usernames
+# requires WP_USERNAME and WP_KEY environment variables
+
+# echo "WP_USERNAME: $WP_USERNAME"
+# echo "WP_KEY: $WP_KEY"
+
+# PROTOCOL="http"
+PROTOCOL="https"
+BASE_URL="$PROTOCOL://toddbooth.com/wp-json/wp/v2"
+response=$(curl -X GET -u "${WP_USERNAME}:${WP_KEY}" ${BASE_URL}/users)
+
+# Check if the following user name exists
+TEST_NAME="d7001dbogpat4"
+
+echo "checking for $TEST_NAME"
+
+# echo $response
+
+# Process JSON items without pipes
+items=$(jq -c '.[]' <<< "$response")
+
+# Split items into an array
+IFS=$'\n' read -rd '' -a items_array <<< "$items"
+
+# Loop through the items
+for item in "${items_array[@]}"; do
+    name=$(jq -r '.name' <<< "$item")
+    # echo "$name"
+    if [ "$name" = "$TEST_NAME" ]; then
+        echo "name exists: $name"
+        exit 0
+    fi
+done
+echo "name does not exist: $TEST_NAME"
+exit 1
